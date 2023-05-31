c5f1d748b7286c06276cb4613f280a4472e3c777
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-05-31 20:39:56 +0530

    n=int(input()) while n>=9:     s=0     while n!=0:         r=n%10         s=s+r*r         n=n//10     n=s if n==1:     print(True) else:     print(False)

--- happy_number.py
+++ happy_number.py
@@ -1,12 +1,12 @@
 n=int(input())
-while n>9:
+while n>=9:
     s=0
     while n!=0:
         r=n%10
         s=s+r*r
         n=n//10
     n=s
-if n==1 or n==7:
+if n==1:
     print(True)
 else:
     print(False)
