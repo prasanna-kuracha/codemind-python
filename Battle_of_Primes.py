e78bae398269b84539058c3c1592768be1d48736
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-06-01 22:27:07 +0530

    def fun(n):     for i in range(2,n//2+1):         if n%i==0:             return 0     return 1 n=int(input()) m=int(input()) f=n+m for i in range(1,f+1):     k=f+i     if fun(k)==1:         print(i)         break

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,14 +1,14 @@
 def fun(n):
-    for i in range(2,(n//2)+1):
+    for i in range(2,n//2+1):
         if n%i==0:
             return 0
-    return 1
-    
-a=int(input())
-b=int(input())
-c=a+b
-for i in range(1,c+1):
-    x=c+i
-    if fun(x)==1:
-        print(i,end=" ")
-        break;
+    return 1        
+n=int(input())
+m=int(input())
+f=n+m
+for i in range(1,f+1):
+    k=f+i
+    if fun(k)==1:
+        print(i)
+        break
+        
