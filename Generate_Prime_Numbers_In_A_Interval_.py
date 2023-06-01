53f2e4f46e9e444ab4af6165584f9fbe1db6b118
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-06-01 22:30:32 +0530

    def fun(n):     for i in range(2,n//2+1):         if n%i==0:             return 0     return 1 n=int(input()) m=int(input()) for i in range(n+1,m+1):     x=i     if fun(x)==1:         print(x)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,11 +1,12 @@
 def fun(n):
-    for i in range(2,(n//2)+1):
+    for i in range(2,n//2+1):
         if n%i==0:
             return 0
-    return 1
+    return 1        
+n=int(input())
+m=int(input())
+for i in range(n+1,m+1):
+    x=i
+    if fun(x)==1:
+        print(x)
     
-a=int(input())
-b=int(input())
-for i in range(a+1,b):
-    if fun(i)==1:
-        print(i)
