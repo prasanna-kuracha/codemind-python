4a639b98c765bc23c9d811d89409408aacd87cc5
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-06-02 20:40:41 +0530

    def fun(n):     for i in range(2,n//2+1):         if n%i==0:             return 0     return 1 n=int(input()) a=list(map(int,input().split())) k=int(input()) c=0 for i in a:     if i==1:         continue     else:         if fun(i)==1 and i>=k:             c=c+1 print(c)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,16 @@
+def fun(n):
+    for i in range(2,n//2+1):
+        if n%i==0:
+            return 0
+    return 1        
+n=int(input())
+a=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in a:
+    if i==1:
+        continue
+    else:
+        if fun(i)==1 and i>=k:
+            c=c+1
+print(c)            
