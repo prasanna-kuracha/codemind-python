9fc24259a8609480bd2146f4230eb7c8a6f8b9b2
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-06-02 20:06:50 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] for i in a:     if i not in b:         if i%2==0:             b.append(i)         else:             break print(sum(b))

---
+++ sum_of_array__elements_upto__first_odd_number.py
@@ -0,0 +1,10 @@
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+for i in a:
+    if i not in b:
+        if i%2==0:
+            b.append(i)
+        else:
+            break
+print(sum(b))        
