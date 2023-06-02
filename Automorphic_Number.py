7bb14412c6bb20fc70645abd57384ee285653292
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-06-02 14:12:27 +0530

    n=int(input()) k=n*n c=0 while n!=0:     r=n%10     m=k%10     if r!=m:         c=1         break     n=n//10     k=k//10 if c==1:     print('Not an Automorphic Number') else:     print('Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,15 +1,15 @@
 n=int(input())
 k=n*n
-c=1
+c=0
 while n!=0:
     r=n%10
     m=k%10
     if r!=m:
-        c=0
+        c=1
         break
     n=n//10
     k=k//10
-if c==0:
+if c==1:
     print('Not an Automorphic Number')
 else:
     print('Automorphic Number')
