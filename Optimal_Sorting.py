3dd0be662ababfe1449286dbb2b68c730797ea92
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-06-04 15:16:48 +0530

    n=int(input()) for i in range(1,n+1):     m=int(input())     a=list(map(int,input().split()))     b=sorted(a)     if a==b:         print('0')     else:         print(max(b)-min(b))

--- Optimal_Sorting.py
+++ Optimal_Sorting.py
@@ -3,7 +3,7 @@ for i in range(1,n+1):
     m=int(input())
     a=list(map(int,input().split()))
     b=sorted(a)
-    if b==a:
+    if a==b:
         print('0')
     else:
         print(max(b)-min(b))
