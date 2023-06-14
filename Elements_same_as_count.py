b11e329755f8350fab9ec7084ca83979d39a424f
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-06-14 21:57:30 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] s=0 for i in a:     if i==a.count(i):         if i not in b:             b.append(i)             s=1 if s==1:     print(*b) else:     print('-1')

--- Elements_same_as_count.py
+++ Elements_same_as_count.py
@@ -1,13 +1,13 @@
 n=int(input())
 a=list(map(int,input().split()))
-s=0
 b=[]
+s=0
 for i in a:
     if i==a.count(i):
         if i not in b:
             b.append(i)
             s=1
 if s==1:
-    print(*b,end=" ")
+    print(*b)
 else:
     print('-1')
