9d1fd0be1272897feffb4d2f374f7a8f11df2028
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-07-04 18:08:28 +0530

    n=input() k='aeiou' s="" for i in range(0,len(n)):     if len(s)==0:         if n[i] in k:             s=s+'V'         else:             s=s+'C'     elif n[i] in k:         if s[len(s)-1]!='V':             s=s+'V'     else:         if s[len(s)-1]!='C':             s=s+'C' print(s)

--- Vowel_Consonant_Seqeuence.py
+++ Vowel_Consonant_Seqeuence.py
@@ -1,16 +1,17 @@
 n=input()
 k='aeiou'
 s=""
-for i in n:
-    if i in k:
-        s=s+'V'
+for i in range(0,len(n)):
+    if len(s)==0:
+        if n[i] in k:
+            s=s+'V'
+        else:
+            s=s+'C'
+    elif n[i] in k:
+        if s[len(s)-1]!='V':
+            s=s+'V'
     else:
-        s=s+'C'
-p=s[0]
-q=s[0]
-for i in range(1,len(s)):
-    if p!=s[i]:
-        p=s[i]
-        q=q+s[i]
-print(q)
-    
+        if s[len(s)-1]!='C':
+            s=s+'C'
+print(s)
+        
