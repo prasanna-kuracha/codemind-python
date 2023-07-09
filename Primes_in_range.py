0dd51d7184dfdd32282e0931f641787d743fa596
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-07-10 00:38:35 +0530

    def fun(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     return 1 n=int(input()) m=int(input()) c=0 for i in range(n,m+1):     if i==1:         continue     elif fun(i)==1:         c=c+1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,4 +1,4 @@
-def prime(n):
+def fun(n):
     for i in range(2,int(n**0.5)+1):
         if n%i==0:
             return 0
@@ -9,7 +9,6 @@ c=0
 for i in range(n,m+1):
     if i==1:
         continue
-    elif prime(i)==1:
+    elif fun(i)==1:
         c=c+1
 print(c)
-        
